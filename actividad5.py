b0ad001e18132a148b90c19fc367e8286df8405f
aufus04 <bogadoaugusto@iresm.edu.ar>
2024-12-13 01:00:13 -0300

    actividad5.py

---
+++ actividad5.py
@@ -0,0 +1,9 @@
+class Personaje:#creamos la clase Personaje
+    real = False
+
+harry_potter = Personaje()#armar una instancia de clase Personaje
+
+#se define atributos de instancia
+harry_potter.especie = "Humano"
+harry_potter.magico = True
+harry_potter.edad = 17
